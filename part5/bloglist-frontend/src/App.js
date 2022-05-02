061e2dce081d59de4e66c5000111972b984b5939
Tri Ho <minhtri191193@gmail.com>
2022-05-02 13:37:48 -0700

    complete part 5a - implements login functionality in front-end

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,17 +1,184 @@
 import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import Notification from "./components/Notification";
 import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const LOGGED_IN_USER_KEY = "loggedBlogAppUser";
 
 const App = () => {
+  /**
+   * States
+   */
+
+  // all blog
   const [blogs, setBlogs] = useState([]);
+  // new blog
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newURL, setNewURL] = useState("");
+  // logged-in user
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  // notification
+  const [notification, setNotification] = useState(null);
+
+  /**
+   * Effects
+   */
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem(LOGGED_IN_USER_KEY);
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  /**
+   * Event Handlers
+   */
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      // login operation
+      const user = await loginService.login({ username, password });
+      // notify successful login
+      notify(`Login successfully! Hello ${user.name}`);
+      // update the `user` state
+      setUser(user);
+      // set user's token into the `token` variable in blogService module
+      blogService.setToken(user.token);
+      // make the user persistence
+      window.localStorage.setItem(LOGGED_IN_USER_KEY, JSON.stringify(user));
+      // reset fields
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      notify("wrong username or password", "alert");
+    }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem(LOGGED_IN_USER_KEY);
+    setUser(null);
+  };
+
+  const notify = (message, type = "info") => {
+    setNotification({ message, type });
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
+
+  const addBlog = async (event) => {
+    event.preventDefault();
+    try {
+      const savedBlog = await blogService.create({
+        title: newTitle,
+        author: newAuthor,
+        url: newURL,
+      });
+      // notify message
+      notify(`a new blog ${savedBlog.title} by ${savedBlog.author} added`);
+      // update the blogs state on front-end (browser)
+      setBlogs(blogs.concat(savedBlog));
+    } catch (exception) {
+      notify("fails to add new blog", "alert");
+    }
+
+    setNewTitle("");
+    setNewAuthor("");
+    setNewURL("");
+  };
+
+  /**
+   * views to render
+   */
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username{" "}
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password{" "}
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        title:{" "}
+        <input
+          type="text"
+          value={newTitle}
+          onChange={({ target }) => setNewTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:{" "}
+        <input
+          type="text"
+          value={newAuthor}
+          onChange={({ target }) => setNewAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:{" "}
+        <input
+          type="text"
+          value={newURL}
+          onChange={({ target }) => setNewURL(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {notification && <Notification notification={notification} />}
+        {loginForm()}
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      {notification && <Notification notification={notification} />}
+      <p>
+        {user.name} logged in{" "}
+        <button type="button" onClick={handleLogout}>
+          logout
+        </button>
+      </p>
+      <h2>create new</h2>
+      {blogForm()}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,11 +1,27 @@
 import axios from "axios";
 const baseUrl = "/api/blogs";
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
-const blogsService = { getAll };
+const create = async (newBlog) => {
+  // Note: token is required to create a new blog
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
+
+const blogsService = { getAll, create, setToken };
 
 export default blogsService;
